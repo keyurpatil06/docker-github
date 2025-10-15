3d6234cf2052b382c1810a50deed58a69fe4cc1b
keyurpatil06 <keyurpatil3000@gmail.com>
2025-10-15 17:01:40 +0530

    update test

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../index'); 
+
+let server; // Declare a variable to hold the server instance
+
+beforeAll((done) => {
+    server = app.listen(3000, done); // Start the server before the tests run
+});
+
+afterAll((done) => {
+    server.close(done); // Close the server after the tests are finished
+});
 
 describe('GET /', () => {
     it('should return Hello from Keyur', async () => {
